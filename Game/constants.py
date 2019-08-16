a2563790d0cf775bb2d60fdc73bd287a543d57b1
Glenn Clapp <clappg@jsjcorp.com>
2019-08-16 11:06:41 -0400

    Code sketching commit

--- Game/constants.py
+++ Game/constants.py
@@ -6,6 +6,16 @@ pygame.init()
 GAME_WIDTH = 800
 GAME_HEIGHT = 600
 
+# Scene sizes are number of tiles in the scene.
+SCENE_WIDTH_TILES = 15
+SCENE_HEIGHT_TILES = 15
+
+# Tile resolution
+RES = 64
+
+SCENE_WIDTH = SCENE_WIDTH_TILES * RES
+SCENE_HEIGHT = SCENE_HEIGHT_TILES * RES
+
 # Color definitions
 COLOR_BLACK = (0,0,0)
 COLOR_GREY = (100,100,100)
@@ -15,3 +25,7 @@ DEFAULT_BG = COLOR_GREY
 
 # Sprites
 S_PLAYER = pygame.image.load("data/character.png")
+
+# Tiles
+CAVE_WALL = pygame.image.load("data/cave_wall.png")
+CAVE_FLOOR = pygame.image.load("data/cave_floor.png")
--- Game/main.py
+++ Game/main.py
@@ -1,6 +1,18 @@
 import pygame
 import constants
 
+class struct_tile():
+    def __init__(self, tile_number):
+        # Based on tile number get a tile definition to set its image and
+        # whether it is blocking.
+        # self.block_path = block_path
+
+def map_create():
+    new_map = [[struct_tile(False) for y in range(0,30)]for x in range(0,30)]
+    new_map[10][10].block_path = True
+    new_map[10][15].block_path = True
+    return(new_map)
+        
 def quit_nicely():
     pygame.display.quit()
     pygame.quit()
@@ -16,7 +28,7 @@ def draw_game(SURFACE_MAIN):
 
     pygame.display.flip()
 
-def game_main_loop(SURFACE_MAIN):
+def game_main_loop(SURFACE_MAIN, game_map):
     game_quit = False
     fpsClock = pygame.time.Clock()
     
@@ -36,9 +48,10 @@ def game_initialize():
 
     SURFACE_MAIN = pygame.display.set_mode((constants.GAME_WIDTH,
                                             constants.GAME_HEIGHT))
+    game_map = map_create()
 
-    return(SURFACE_MAIN)
+    return(SURFACE_MAIN, game_map)
 
 if __name__ == "__main__":
-    SURFACE_MAIN = game_initialize()
-    game_main_loop(SURFACE_MAIN)
+    SURFACE_MAIN, game_map = game_initialize()
+    game_main_loop(SURFACE_MAIN, game_map)
